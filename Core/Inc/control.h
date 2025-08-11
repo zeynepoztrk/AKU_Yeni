9498dc7d9b788967013ce055059963aa8dac7055
yagiz20Z <aliyagizuz@gmail.com>
2025-08-11 15:08:00 +0300

    freertos.c üzerinde değişiklikler

---
+++ Core/Inc/control.h
@@ -0,0 +1,20 @@
+/*
+ * control.h
+ *
+ *  Created on: Aug 11, 2025
+ *      Author: aliya
+ */
+
+#ifndef INC_CONTROL_H_
+#define INC_CONTROL_H_
+
+#include "main.h"
+#include "BMI088.h"
+
+
+
+
+
+
+
+#endif /* INC_CONTROL_H_ */
---
+++ Core/Src/control.c
@@ -0,0 +1,9 @@
+/*
+ * control.c
+ *
+ *  Created on: Aug 11, 2025
+ *      Author: aliya
+ */
+
+#include "control.h"
+
