262fe614937f10e3785bc77e1559caac9d835f11
zeynepoztrk <nochuct08@gmail.com>
2025-08-14 17:13:26 +0300

    control.c ve controol.h değiştirildi

--- Core/Inc/control.h
+++ Core/Inc/control.h
@@ -8,8 +8,21 @@
 #ifndef INC_CONTROL_H_
 #define INC_CONTROL_H_
 
+#include "stdlib.h"
+#include "stdio.h"
 #include "main.h"
 #include "BMI088.h"
+#include "tim.h"
+#include "i2c.h"
+
+#define acc_adress 0x18 << 1
+#define gyro_adress 0x69 << 1
+#define DELAY 100
+#define CENTER 0
+#define HATA
+#define Kp_x
+#define Kp_y
+#define referans 0
 
 
 
